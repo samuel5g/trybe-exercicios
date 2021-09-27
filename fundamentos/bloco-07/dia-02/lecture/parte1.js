e3979f16f9ea3f655227fd45420178d0cdc69bf8
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-27 18:23:05 -0300

    Conclui parte 1 lecture

--- fundamentos/bloco-07/dia-02/lecture/parte1.js
+++ fundamentos/bloco-07/dia-02/lecture/parte1.js
@@ -1,7 +1,9 @@
-const createObject = (object, chave, value) => {
-  return object =  {
-    chave: value
-  }
+// Agora, para praticar, crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
+
+const createObjects  = (objeto, chave, valor) => {
+  objeto = new Object();
+  objeto[chave] = valor
+  return objeto; 
 }
 
-console.log(createObject('samuel', 'idade', 21)); 
+console.log(createObjects('human', 'name', 'samuel'));
