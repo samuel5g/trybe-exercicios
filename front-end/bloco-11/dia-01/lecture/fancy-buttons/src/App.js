7687434537f219ebb452fdac581e1693390ec027
Samuel Gonçalves <samuelmonteiro21@gmail.com>
2021-11-04 12:07:58 -0300

    Conclui exercicio state lecture

--- front-end/bloco-11/dia-01/lecture/fancy-buttons/src/App.js
+++ front-end/bloco-11/dia-01/lecture/fancy-buttons/src/App.js
@@ -5,29 +5,41 @@ class App extends React.Component {
   constructor () {
     super();
 
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
+    }
+
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
   }
 
   handleClick1() {
-    console.log('alou');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }))
   }
   
   handleClick2() {
-    console.log('alou de novo');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }))
   }
   
   handleClick3() {
-    console.log('alou mais uma vez');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }))
   }
 
   render() {
     return (
       <p>
-        <button onClick={this.handleClick1}>My Button</button>
-        <button onClick={this.handleClick2}>My Other Button</button>
-        <button onClick={this.handleClick3}>My Another Button</button>
+        <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
+        <button onClick={this.handleClick2}>{this.state.numeroDeCliques2}</button>
+        <button onClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
       </p>
     );
   }
