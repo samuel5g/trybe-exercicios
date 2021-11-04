885a20b603ef0ef72edacf386b9898bad4463357
Samuel Gonçalves <samuelmonteiro21@gmail.com>
2021-11-04 11:37:49 -0300

    Conclui exercicio constructor lecture

--- front-end/bloco-11/dia-01/lecture/fancy-buttons/src/App.js
+++ front-end/bloco-11/dia-01/lecture/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import React from 'react';
 
-function handleClick1() {
-  console.log('alou');
-}
-
-function handleClick2() {
-  console.log('alou de novo');
-}
-
-function handleClick3() {
-  console.log('alou mais uma vez');
-}
-
 class App extends React.Component {
+  constructor () {
+    super();
+
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  handleClick1() {
+    console.log('alou');
+  }
+  
+  handleClick2() {
+    console.log('alou de novo');
+  }
+  
+  handleClick3() {
+    console.log('alou mais uma vez');
+  }
+
   render() {
     return (
       <p>
-        <button onClick={handleClick1}>My Button</button>
-        <button onClick={handleClick2}>My Other Button</button>
-        <button onClick={handleClick3}>My Another Button</button>
+        <button onClick={this.handleClick1}>My Button</button>
+        <button onClick={this.handleClick2}>My Other Button</button>
+        <button onClick={this.handleClick3}>My Another Button</button>
       </p>
     );
   }
