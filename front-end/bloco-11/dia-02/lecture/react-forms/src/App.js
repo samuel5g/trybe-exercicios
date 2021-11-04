f1d777418579a4e5e922089367b0f91472bb3add
Samuel Gonçalves <samuelmonteiro21@gmail.com>
2021-11-04 20:22:32 -0300

    :sparkles: Deixa componentes mais genericos

--- front-end/bloco-11/dia-02/lecture/react-forms/src/App.js
+++ front-end/bloco-11/dia-02/lecture/react-forms/src/App.js
@@ -8,22 +8,39 @@ class App extends React.Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
+      name: '',
       email: '',
     }
   }
 
   handleChange(event) {
     this.setState({
-      email: event.target.value,
+      [event.target.name]: event.target.value,
     })
   }
   render () {
-    const { email } = this.state;
+    const { email, name } = this.state;
     return (
       <form>
-        <select></select>
-        <input type="text"></input>
-        <input type="email" value={ email } onChange={ this.handleChange }></input>
+        <select>
+          <option value="">Casa</option>
+          <option value="">Apartamento</option>
+          <option value="">Mansão</option>
+        </select>
+        <input 
+          type="text"
+          name="name"
+          placeholder="seu nome aqui"
+          value={ name }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="seu email aqui"
+          value={ email }
+          onChange={ this.handleChange }
+        />
         <textarea></textarea>
       </form>
     );
